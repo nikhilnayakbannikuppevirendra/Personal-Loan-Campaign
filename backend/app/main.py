0beb2c8b96a286b1c14adf0dc434852847c54af6
nikhilnayakbannikuppevirendra <nikhilnayakbannikuppevirendra@gmail.com>
2026-01-15 17:39:08 +0530

    CORS Fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.health import router as health_router
 from app.routes.predict import router as predict_router
 
 app = FastAPI(
     title="Personal Loan Campaign API",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "https://personal-loan-frontend.onrender.com",  # replace with your deployed FE URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # "*" allows all origins. Safe for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(health_router)
